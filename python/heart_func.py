ef73db097cbafa16d36711f944b996a177a724da
“luola” <ll63702168@qq.com>
2020-01-16 00:00:10 +0800

    func

--- python/heart_func.py
+++ python/heart_func.py
@@ -3,7 +3,7 @@ import os
 
 
 def heart():
-    for char in "Dear I love you forever".split():
+    for char in "will be ok".split():
         lst_all_line = []
         for y in range(12, -12, -1):
             lst_line = []
@@ -22,7 +22,7 @@ def heart():
 
 
 def heart2():
-    for s in "Dear I love you forever".split():
+    for s in "will be ok".split():
         print(f'{os.linesep}'.join([''.join([(s[(x - y) % len(s)] if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (
                 x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(12, -12, -1)]))
         time.sleep(1)
